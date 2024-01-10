f7fa3710c144f88bfbe5e3bea1b6cfcbafda31bf
Rohitkr2510 <rohit70_soe@jnu.ac.in>
2024-01-10 22:22:45 +0530

    all files

---
+++ util/database.js
@@ -0,0 +1,11 @@
+const Sequelize = require('sequelize');
+
+const sequelize = new Sequelize(
+    'node-complete',
+     'root',
+      'Rohit2510@', {
+    dialect: 'mysql',
+    host: 'localhost'
+});
+
+module.exports = sequelize;
